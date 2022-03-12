4b95b84dfd14589a900fd8fd154bb17908a6b46b
Redchrysanthemum <98815579+Redchrysanthemum@users.noreply.github.com>
2022-03-12 12:45:10 +0900

    Create Movies차트.js

---
+++ Movies차트.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  // async-await를 이용한 작성
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  // .then을 이용한 작성
+  /*  useEffect(() => {
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []); */
+  return (
+    <div>
+      {loading ? (
+        <h1>로딩중...</h1>
+      ) : (
+        <div>
+          {movies.map((argument) => (
+            <div key={movies.id}>
+              <h2>{argument.title}</h2>
+              <p>{argument.summary}</p>
+              <ul>
+                {movies.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
